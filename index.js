039ba276e62d5feee07a3f2dbaf0fec642350894
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-06-26 11:15:30 -0300

    OK

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ let consulta = {
   horario: "",
 };
 
+let indiceAtualizar;
+let atributoAtualizar;
 let opcao = 0;
-let passoConsulta = 0; // Variável para controlar o passo do processo de inserção de consulta
+let step = 0;
 
 console.log(
   `Qual operação você deseja fazer ? 
@@ -15,34 +17,62 @@ console.log(
   2-Listar consultas 
   3-Atualizar dados 
   4-Cancelar consulta
-`);
+`
+);
 
 process.stdin.on("data", function (data) {
   let input = data.toString().trim();
-  
+
   if (!opcao) {
     opcao = Number(input);
-    switch (opcao){
-        case 1:
-       process.stdout.write("Qual o nome do paciente? \n");
+
+    switch (opcao) {
+      case 1:
+        process.stdout.write("Qual o nome do paciente? \n");
+        break;
+      case 2:
+        console.log("Listar consultas:");
+        bancoconsulta.forEach((consulta, index) => {
+          console.log(`Consulta ${index}:`);
+          console.log(consulta);
+        });
+        opcao = 0;
+        break;
+      case 3:
+        console.log("Atualizar dados de consulta:");
+        console.log("Escolha a consulta que deseja atualizar:");
+        bancoconsulta.forEach((consulta, index) => {
+          console.log(`${index}: ${consulta.nomepaciente}`);
+        });
+        break;
+      case 4:
+        console.log("Cancelar consulta:");
+        console.log("Escolha a consulta que deseja cancelar:");
+        bancoconsulta.forEach((consulta, index) => {
+          console.log(`${index}: ${consulta.nomepaciente}`);
+        });
+        break;
+      default:
+        console.log("Opção inválida.");
+        opcao = 0;
+        break;
     }
   } else {
     switch (opcao) {
       case 1:
-        if (passoConsulta === 0) {
+        if (step === 0) {
           consulta.nomepaciente = input;
-          passoConsulta++;
+          step++;
           process.stdout.write("Qual o nome do médico? \n");
-        } else if (passoConsulta === 1) {
+        } else if (step === 1) {
           consulta.nomemedico = input;
-          passoConsulta++;
+          step++;
           process.stdout.write("Qual a data da consulta? \n");
-        } else if (passoConsulta === 2) {
-            process.stdout.write("Qual horário da consulta? \n");
+        } else if (step === 2) {
           consulta.data = input;
-          passoConsulta++;
-        } else if (passoConsulta === 3) {
-          
+          step++;
+          process.stdout.write("Qual horário da consulta? \n");
+        } else if (step === 3) {
           consulta.horario = input;
           console.log("Consulta inserida com sucesso!");
           bancoconsulta.push({
@@ -58,21 +88,69 @@ process.stdin.on("data", function (data) {
             data: "",
             horario: "",
           };
-          passoConsulta = 0;
+          step = 0;
           opcao = 0;
         }
         break;
-      case 2:
-        console.log("Listar consultas:");
-        console.log(bancoconsulta);
-        opcao = 0;
-        break;
       case 3:
-        console.log("Opção de atualização de dados selecionada.");
-        opcao = 0;
+        if (!indiceAtualizar) {
+          indiceAtualizar = Number(input);
+          console.log(`Você escolheu atualizar a consulta ${indiceAtualizar}.`);
+          console.log("Qual atributo você deseja atualizar?");
+          console.log("1 - Nome do paciente");
+          console.log("2 - Nome do médico");
+          console.log("3 - Data da consulta");
+          console.log("4 - Horário da consulta");
+        } else if (!atributoAtualizar) {
+          atributoAtualizar = Number(input);
+          switch (atributoAtualizar) {
+            case 1:
+              console.log("Digite o novo nome do paciente:");
+              break;
+            case 2:
+              console.log("Digite o novo nome do médico:");
+              break;
+            case 3:
+              console.log("Digite a nova data da consulta:");
+              break;
+            case 4:
+              console.log("Digite o novo horário da consulta:");
+              break;
+            default:
+              console.log("Atributo inválido.");
+              opcao = 0;
+              indiceAtualizar = undefined;
+              atributoAtualizar = undefined;
+              break;
+          }
+        } else {
+          switch (atributoAtualizar) {
+            case 1:
+              bancoconsulta[indiceAtualizar].nomepaciente = input;
+              break;
+            case 2:
+              bancoconsulta[indiceAtualizar].nomemedico = input;
+              break;
+            case 3:
+              bancoconsulta[indiceAtualizar].data = input;
+              break;
+            case 4:
+              bancoconsulta[indiceAtualizar].horario = input;
+              break;
+          }
+          console.log("Consulta atualizada com sucesso!");
+          console.log(bancoconsulta[indiceAtualizar]);
+          indiceAtualizar = undefined;
+          atributoAtualizar = undefined;
+          opcao = 0;
+        }
         break;
       case 4:
-        console.log("Opção de cancelamento de consulta selecionada.");
+        let indiceRemocao = Number(input);
+        console.log(`Consulta ${indiceRemocao} cancelada.`);
+        bancoconsulta.splice(indiceRemocao, 1);
+        console.log("Consultas restantes:");
+        console.log(bancoconsulta);
         opcao = 0;
         break;
       default:
